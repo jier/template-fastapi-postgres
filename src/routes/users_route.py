27c9190207f93d4ae65f1d7b4c27c04bc241e76f
Fernando Crozetta <fernando@czetta.com>
2023-11-04 20:39:03 +0100

    adding read users

--- src/routes/users_route.py
+++ src/routes/users_route.py
@@ -1,18 +1,25 @@
-from fastapi import Depends
+from fastapi import Depends, Response
 from fastapi.routing import APIRouter
+from models import user_model
 from schemas.user_schema import User, UserCreate
 from services import database_service as dbs
-#from services.database_service import d
+from sqlalchemy.orm import Session
 from config.database_config import get_db
 
 app = APIRouter(prefix="/users", tags=["Users"])
 
 
 @app.get("/", response_model=list[User])
-async def get_all_users(db = Depends(get_db)):
+async def get_all_users(db: Session = Depends(get_db)):
     return dbs.get_all_users(db)
 
 
 @app.post("/")
 async def create_user(input_user: UserCreate):
     ...
+
+
+@app.get("/{user_id}", response_model=User)
+async def get_user_by_id(user_id: int, db=Depends(get_db)):
+    result = dbs.get_user_by_id(db, user_id)
+    return result if result else Response(status_code=404)
--- src/services/database_service.py
+++ src/services/database_service.py
@@ -2,6 +2,12 @@ from schemas import user_schema
 from models import user_model
 from sqlalchemy.orm import Session
 
-async def get_all_users(db: Session, skip:int = 0, limit:int=100) -> list[user_model.User]:
+
+def get_all_users(
+    db: Session, skip: int = 0, limit: int = 100
+) -> list[user_model.User]:
     return db.query(user_model.User).offset(skip).limit(limit).all()
-    
+
+
+def get_user_by_id(db: Session, user_id: int) -> user_model.User | None:
+    return db.query(user_model.User).get(user_id)
